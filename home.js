e2826434180d2f8cb48337d86814b40cd01b9f2a
Eric Chrsitine <ericjchristine@gmail.com>
2020-11-14 22:48:00 -0700

    functional UI, needs refactoring

---
+++ home.js
@@ -0,0 +1,10 @@
+console.log('connected')
+
+let logo = document.querySelector('img')
+console.log(logo)
+
+logo.addEventListener('click', function (e) {
+    // e.preventDefault()
+    console.log(e)
+})
+
