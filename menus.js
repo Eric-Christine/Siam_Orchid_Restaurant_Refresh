3f6001077b7f3a2ce62c0c2143fc3b947a5be9af
Eric Chrsitine <ericjchristine@gmail.com>
2021-05-29 21:48:17 -0600

    added beverages section in menu

--- menus.js
+++ menus.js
@@ -6,12 +6,16 @@ hrElements.forEach(element => element.setAttribute("class", "hr-items"))
 var liElements= document.body.querySelectorAll('main li')
 liElements.forEach(element=> element.setAttribute("class", "list-items" ))
 
+var iElements= document.body.querySelectorAll('ul i')
+iElements.forEach(element=> element.setAttribute("class", "italic-items" ))
+
 // console.log(hrElements)
 
 
 // // Give all elements start CSS attribute
 onScroll()
 onScroll2()
+onScroll3()
 
 function onScroll() {
     var elements;
@@ -58,6 +62,35 @@ function onScroll2() {
         if (positionFromTop - windowHeight <= 0) {
           element.classList.add('float-up');
           element.classList.remove('hidden');
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  
+    init();
+    checkPosition();
+  };
+
+
+function onScroll3() {
+    var elements;
+    var windowHeight;
+  
+    function init() {
+      elements = document.querySelectorAll('.italic-items');
+      windowHeight = window.innerHeight;
+    }
+  
+    function checkPosition() {
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add('start');
+          element.classList.remove('italic-items');
         }
       }
     }
